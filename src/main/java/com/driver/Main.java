cf55714475d9dd33a0f4216c8c95467e6150f97e
Durvesh Kumar <durvesh88999@gamil.com>
2026-09-16 03:22:02 +0530

    create getter and setter method

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r = new RWOnly();
+
+        r.setter("Durvesh kumar");
+
+        System.out.println(r.getter());
+    }
 }
